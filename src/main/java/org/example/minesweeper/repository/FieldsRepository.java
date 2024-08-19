b830d47ebb396c7a11faed51f6e27ba50240e641
paren_s_povyazkoy <109079912+parnysha@users.noreply.github.com>
2024-08-19 22:16:43 +0300

    Update FieldsRepository.java

--- src/main/java/org/example/minesweeper/repository/FieldsRepository.java
+++ src/main/java/org/example/minesweeper/repository/FieldsRepository.java
@@ -4,6 +4,7 @@ import org.example.minesweeper.dto.FieldsString;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.query.Param;
 
 
 @Repository
