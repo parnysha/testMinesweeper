6dab8f2cd335613f3e8fd3d063abfdf1ca79fce5
paren_s_povyazkoy <109079912+parnysha@users.noreply.github.com>
2024-09-30 23:33:21 +0300

    Update GameController.java

--- src/main/java/org/example/minesweeper/controller/GameController.java
+++ src/main/java/org/example/minesweeper/controller/GameController.java
@@ -15,15 +15,12 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api")
 public class GameController {
     private final GameService gameService;
-
     @PostMapping("/new")
     public JSonPlayer createUser(@RequestBody JSonReqNew reqNew) {
         return gameService.newGameRequest(reqNew.getWidth(), reqNew.getHeight(), reqNew.getMines_count());
     }
-
     @PostMapping("/turn")
     public JSonPlayer turnUser(@RequestBody JSonReqTurn reqNew) {
         return gameService.gameTurnRequest(reqNew.getGame_id(),reqNew.getCol(),reqNew.getRow());
     }
-
 }
